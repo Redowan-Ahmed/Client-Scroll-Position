081dffe4b32d3631cac0f54d73654aa6a8d768fd
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2021-01-16 00:34:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", () => {
   let scroll = scrollY;
-  pdata = document.body.scrollHeight - 545;
+  pdata = document.body.scrollHeight;
   mdata = (scroll/pdata)*100;
   document.querySelector('div#progres').style.width = `${mdata}%`;
   console.log(mdata)
